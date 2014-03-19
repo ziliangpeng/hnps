2772e526b4bf3ec79b4d7fd3a36414f81e3ddbeb
Victor Peng <ziliangdotme@gmail.com>
2014-03-19 13:58:27 +1030

    parse json from algolia search

--- algolia.py
+++ algolia.py
@@ -1,14 +1,43 @@
 from urllib2 import urlopen
+import json
 
 URL_PATTERN = 'http://hn.algolia.com/api/v1/items/%d'
 
 
-for i in range(1, 1000):
-    print 'item', i
-    url = URL_PATTERN % i
-    print url
-    try:
-        print urlopen(url).read()
-    except Exception as e:
-        print e
+def save(item_id, json_object):
+    pass
+
+def parse(json_object):
+    item_id = json_object['id']
+
+    obj_cnt = 1
+    for child in json_object['children']:
+        obj_cnt += parse(child)
+
+    save(item_id, json_object)
+    return obj_cnt
+
+
+def is_exist(item_id):
+    pass
+
+
+total_found = 0
+for i in range(1, 10):
+    print 'item', i,
+    if is_exist(i):
+        print 'existed',
+    else:
+        url = URL_PATTERN % i
+        try:
+            raw_json = urlopen(url).read()
+            json_object = json.loads(raw_json)
+            item_cnt = parse(json_object)
+            print item_cnt, 'found',
+            total_found += item_cnt
+            print 'total found:', total_found,
+        except Exception as e:
+            print e,
+
+    print ''
 
