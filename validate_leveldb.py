496cb434398f39b6ec19ba37acb779c5edce2b7a
Victor Peng <ziliangdotme@gmail.com>
2014-03-24 22:17:55 +0930

    configurable io

--- validate_leveldb.py
+++ validate_leveldb.py
@@ -16,15 +16,27 @@ def complete_thread():
             parent_id = None
 
         if parent_id != None:
-            print k, parent_id,
             try:
                 db.Get(str(parent_id))
                 parent_exist = True
             except KeyError:
                 parent_exist = False
-            print parent_exist
+            #print parent_exist
             if not parent_exist:
-                print 'fucked!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+                print k, parent_id
+                #print 'fucked' + '!' * 50
 
-        
-complete_thread()
+
+
+def get_urls():
+    for k, v in db.RangeIter():
+        obj = json.loads(v)
+        if 'url' in obj and obj['url'] != '':
+            yield obj['url']
+
+if __name__ == '__main__':
+    urls = list(get_urls())
+    for url in urls:
+        print url
+    print len(urls)
+
